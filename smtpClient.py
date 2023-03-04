5a0ecc104f864403868b9e76c4b4d7ae58fcadd1
Smit Kashyap Trivedi <112817199+st4972@users.noreply.github.com>
2023-03-04 12:43:11 -0500

    Update smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -10,6 +10,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
 
     # Fill in start
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((mailserver, port))
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
@@ -27,18 +29,37 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
+
+    mailFrom = 'MAIL FROM "SMIT.ALMIGHTY@GMAIL.COM"'
+    clientSocket.send(mailFrom.encode())
+    recv2 = clientSocket.recv(1024).decode()
+
     # Fill in end
 
     # Send RCPT TO command and handle server response.
     # Fill in start
+
+    rcpt_to = 'RCPT TO "rutudave777@gmail.com"'
+    clientSocket.send((rcpt_to.encode()))
+    recv3 = clientSocket.recv(1024).decode()
+
     # Fill in end
 
     # Send DATA command and handle server response.
     # Fill in start
+
+    clientSocket.send((DATA.encode()))
+    recv4= clientSocket.recv(1024).decode()
+
+
     # Fill in end
 
     # Send message data.
     # Fill in start
+
+
+
+
     # Fill in end
 
     # Message ends with a single period, send message end and handle server response.
